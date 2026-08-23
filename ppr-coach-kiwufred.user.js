928917c0bc944d40e2595d6252eed4ec84040516
kins4fred <138931665+kins4fred@users.noreply.github.com>
2026-08-23 05:03:24 -0700

    Add files via upload

---
+++ ppr-coach-kiwufred.user.js
@@ -0,0 +1,2 @@
+// @updateURL    https://raw.githubusercontent.com/kiwufred/ppr-coach/main/ppr-coach-kiwufred.user.js
+// @downloadURL  https://raw.githubusercontent.com/kiwufred/ppr-coach/main/ppr-coach-kiwufred.user.js
